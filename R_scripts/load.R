5c119defca56d66cffcc493c4d9dfb995cf895ba
maxconway <conway.max1@gmail.com>
2013-07-24 23:34:25 +0100

    Now shows fluxes nicely

---
+++ R_scripts/load.R
@@ -0,0 +1,2 @@
+#currently just use results.Rnw
+GDMO.M.reactions=read.delim('./geo_m_react.tsv',)
---
+++ R_scripts/show_fluxes.R
@@ -0,0 +1,30 @@
+require(gplots)
+require(sybil)
+require(lpSolveAPI)
+
+SYBIL_SETTINGS("SOLVER","lpSolveAPI")
+
+normalize <- function(x){
+	result <- sign(x) * log10(abs(x));
+	result[is.nan(result)] <- 0;
+	return(result);
+}
+
+mod <- readTSVmod(reactList='geo_m_react.tsv',remUnusedMetReact=FALSE)
+obj_coef(mod)[match(c('biomass','EX_fe2(e)'),react_id(mod))] <- c(100,1)
+
+setNodeColorRule(met,
+								 node.attribute.name='flux',
+								 control.points=c(-2,0,2),
+								 colors=col2hex(c('black','red','white','green','black')),
+								 mode='interpolate'
+)
+
+t <- GDMO.M.chromosomes[order(GDMO.M.chromosomes$pos),grep(pattern='Gmet_.*',colnames(GDMO.M.chromosomes))]
+for(i in 1:nrow(t)){
+	chrom<-t[i,]
+	sol <- optimizeProb(object=mod,gene=names(chrom)[chrom==T])
+	setNodeAttributesDirect(met,'flux','numeric',as.character(GDMO.M.reactions[,'abbreviation']),normalize(as.vector(fluxes(sol))))
+	redraw(met)
+	msg(met,paste('timestep',i))
+}
