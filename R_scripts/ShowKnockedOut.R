81bf0ea7b58c7542e4203d24632a2c5b72940007
maxconway <conway.max1@gmail.com>
2013-07-28 13:11:57 +0100

    Functionalized knockout showing

---
+++ R_scripts/ShowKnockedOut.R
@@ -0,0 +1,15 @@
+ShowKnockedOut <- function(cytoscapeWindow, chromosomes, reactions){
+	setNodeColorRule(cytoscapeWindow,
+									 node.attribute.name='knockout',
+									 control.points=c('TRUE','FALSE'),
+									 colors=c('red','white'),
+									 mode='lookup'
+	)
+	for(i in chromosomes:nrow(chromosomes)){
+		chrom<-chromosomes[i,]
+		kos <- reactions[reactions$GeneAssociation %in% names(chrom)[chrom==T],c('Abbreviation')]
+		setNodeAttributesDirect(cytoscapeWindow,'knockout','string',as.character(reactions[,'Abbreviation']),as.character(reactions$GeneAssociation %in% names(chrom)[chrom==T]))
+		redraw(cytoscapeWindow)
+		msg(cytoscapeWindow,paste('timestep',i))
+	}
+}
--- R_scripts/show_knocked_out.R
+++ R_scripts/show_knocked_out.R
@@ -1,15 +1,2 @@
-setNodeColorRule(met,
-								 node.attribute.name='knockout',
-								 control.points=c('TRUE','FALSE'),
-								 colors=c('red','white'),
-								 mode='lookup'
-								 )
-
 t <- GDMO.M.chromosomes[order(GDMO.M.chromosomes$pos),grep(pattern='Gmet_.*',colnames(GDMO.M.chromosomes))]
-for(i in 1:nrow(t)){
-	chrom<-t[i,]
-	kos <- GDMO.M.reactions[GDMO.M.reactions$GeneAssociation %in% names(chrom)[chrom==T],c('Abbreviation')]
-	setNodeAttributesDirect(met,'knockout','string',as.character(GDMO.M.reactions[,'Abbreviation']),as.character(GDMO.M.reactions$GeneAssociation %in% names(chrom)[chrom==T]))
-	redraw(met)
-	msg(met,paste('timestep',i))
-}
+ShowKnockedOut(met,t,GDMO.M.reactions)
